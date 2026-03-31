b70c4c7314ac4e1fa84ba334824ee40104d5dd23
Neeraj-gagat <neerajgagat9999@gmail.com>
2026-03-31 23:44:05 +0530

    navbar route issue fixed

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,50 +1,47 @@
-import { useState } from "react"
+import { NavLink } from "react-router-dom"
 
 export const AppBar = () => {
-  const [selected, setSelected] = useState("board")
-
   const navItems = [
-    { name: "Board", key: "board" },
-    { name: "Calendar", key: "calendar" },
-    { name: "Graph", key: "graph" },
-    { name: "Settings", key: "settings" }
+    { name: "Board", path: "/board" },
+    { name: "Calendar", path: "/calendar" },
+    { name: "Graph", path: "/graph" },
+    { name: "Settings", path: "/settings" }
   ]
 
   return (
     <div className="w-full bg-[#091328] border-b border-white/10 fixed top-0 left-0 z-50">
       <div className="flex justify-between items-center px-20 py-3">
         
-        {/* Logo */}
-        <div>
-          <h1 className="text-white text-xl font-semibold tracking-tight">
-            CCSync
-          </h1>
-        </div>
+        <h1 className="text-white text-xl font-semibold tracking-tight">
+          CCSync
+        </h1>
 
-        {/* Navigation */}
-        <div className="flex items-center gap-8 text-[11px] font-medium">
+        <div className="flex items-center gap-8 text-[11px] font-medium uppercase">
           {navItems.map((item) => (
-            <a
-              key={item.key}
-              href={item.key}
-              onClick={() => setSelected(item.key.toLocaleLowerCase())}
-              className={`
-                relative px-2 py-1 transition-all duration-300
-                ${selected === item.key 
-                  ? "text-[#34D4FD]" 
-                  : "text-white/70 hover:text-white"}
-              `}
+            <NavLink
+              key={item.path}
+              to={item.path}
+              end={item.path === "/"} // important for root
+              className={({ isActive }) =>
+                `relative px-2 py-1 transition-all duration-300 ${
+                  isActive
+                    ? "text-[#34D4FD]"
+                    : "text-white/70 hover:text-white"
+                }`
+              }
             >
-              {item.name}
+             {({ isActive }) => (
+                <div className="relative">
+                  {item.name}
 
-              {/* Active underline */}
-              {selected === item.key && (
-                <span className="absolute left-0 bottom-[-6px] w-full h-[2px] bg-[#34D4FD] shadow-[0_0_8px_#34D4FD]" />
+                  {isActive && (
+                    <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-[#34D4FD] shadow-[0_0_8px_#34D4FD]" />
+                  )}
+                </div>
               )}
-            </a>
+            </NavLink>
           ))}
         </div>
-
       </div>
     </div>
   )
